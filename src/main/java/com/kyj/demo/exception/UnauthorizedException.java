c560d6100f1e8e3318a92fff9795c2f5ad436320
kangyuanjia <kangyuanjia@aliyun.com>
2018-11-13 21:57:46 +0800

    Add files via upload

---
+++ src/main/java/com/kyj/demo/exception/UnauthorizedException.java
@@ -0,0 +1,11 @@
+package com.kyj.demo.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String msg) {
+        super(msg);
+    }
+
+    public UnauthorizedException() {
+        super();
+    }
+}
